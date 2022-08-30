ed9d73bedef7ab8f44534b6b96cc3092f9d25f62
rzngnam1402 <legiangnam1402@gmail.com>
2022-08-30 15:07:29 +0700

    Union types

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hesslo");
+function combine(input1, input2) {
+    var res;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        res = input1 + input2;
+    }
+    else {
+        res = input1.toString() + input2.toString();
+    }
+    return res;
+}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log("hesslo");
+function combine(input1: number | string, input2: number | string) {
+  let res;
+
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    res = input1 + input2;
+  } else {
+    res = input1.toString() + input2.toString();
+  }
+  return res;
+}
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
